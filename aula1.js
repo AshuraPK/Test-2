a85ba52facd6e924c9c44906be9330f96bd8dc7e
AshuraPK <168103263+AshuraPK@users.noreply.github.com>
2024-05-23 12:04:40 -0300

    Aula de programação

---
+++ aula1.js
@@ -0,0 +1,38 @@
+//console é para usar o prompt de comando e o log é para escrever 
+console.log("Olá Mundo")
+
+var nome = "Marcia"
+console.log("imprimir", nome)
+console.log(nome)
+
+//variaveis para guarda texto(string), numeros inteiros(int), numeros decimais(float -> guarda até 10 casas decimais) e (double-> guard mais casas decimais)e logico(bool)
+
+var telefone = "11 9999-9999"
+var valormonetario = 54.87
+var valorinteiro = 12
+var verdadeiroFalso = true
+var verdadeiroFalso = false
+//////////////////////////////////////////
+
+var endereco = "rua: das pitangueiras"//string
+var bairro = "vale das noguerias"//string
+var numerodacasa = 401 //int
+var cep = "1335-5080"//string
+var estado = "são paulo"//string
+
+var nome = "marcia" //string
+var email = "marciagisseli@gmail.com" //string
+var telefone = "19981248463"
+var nascimento = "07/05/2000" //string
+var genero = "feminino" //string
+
+var idade=24
+if(idade>18)
+    console.log("você é maior de idade ")
+else
+    console.log("você é menor de idade")
+
+if(idade>18)
+    console.log("vc pode dirigir")
+else
+    console.log("você é menor de idade ainda não pode dirigir")
